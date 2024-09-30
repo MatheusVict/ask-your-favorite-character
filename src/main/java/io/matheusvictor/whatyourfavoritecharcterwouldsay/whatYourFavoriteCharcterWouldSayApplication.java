10c52b42d9eaa33838bb6643ff90ebdb5d30353c
MatheusVict <matheusvictorhenrique@gmail.com>
2024-09-29 22:04:49 -0300

    feat: add open feign

--- src/main/java/io/matheusvictor/whatyourfavoritecharcterwouldsay/whatYourFavoriteCharcterWouldSayApplication.java
+++ src/main/java/io/matheusvictor/whatyourfavoritecharcterwouldsay/whatYourFavoriteCharcterWouldSayApplication.java
@@ -2,8 +2,10 @@ package io.matheusvictor.whatyourfavoritecharcterwouldsay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class whatYourFavoriteCharcterWouldSayApplication {
 
     public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class whatYourFavoriteCharcterWouldSayApplication {
     }
 
 }
+
