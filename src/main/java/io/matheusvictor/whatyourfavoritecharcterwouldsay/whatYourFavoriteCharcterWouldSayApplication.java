808239f9e4361709ddb0785758a51e32fd929335
MatheusVict <matheusvictorhenrique@gmail.com>
2024-09-29 21:11:31 -0300

    inital commit

---
+++ src/main/java/io/matheusvictor/whatyourfavoritecharcterwouldsay/whatYourFavoriteCharcterWouldSayApplication.java
@@ -0,0 +1,13 @@
+package io.matheusvictor.whatyourfavoritecharcterwouldsay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class whatYourFavoriteCharcterWouldSayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(whatYourFavoriteCharcterWouldSayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/matheusvictor/whatyourfavoritecharcterwouldsay/WhatYourFavoriteCharcterWouldSayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.matheusvictor.whatyourfavoritecharcterwouldsay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatYourFavoriteCharcterWouldSayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
